bc77e3797b685b227e05ba20dec4f187d458a99a
Michael Pfundt <47832556+MikePfunk28@users.noreply.github.com>
2024-05-05 20:37:29 -0400

    Quest 4 added, still working on functionality and story.

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -4,6 +4,8 @@ namespace myTiles {
     export const transparency16 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
@@ -35,6 +37,8 @@ namespace myTiles {
             case "transparency16":return transparency16;
             case "myTilebrokenglass":
             case "tile1":return tile1;
+            case "myTile":
+            case "tile2":return tile2;
         }
         return null;
     })
